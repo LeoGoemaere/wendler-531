3a717e9b95ad222bc06208c7d63a12e680c81d84
Goemaere Leo <goemaere.leo@hotmail.fr>
2019-04-29 20:38:01 +0200

    Hnadle display for the primary and secondary exercices

--- src/components/ExercicesSetter.vue
+++ src/components/ExercicesSetter.vue
@@ -1,24 +1,30 @@
 <template>
 	<div>
-	<ExerciceLineItem 
-		v-for="(exercice, index) in this.primaryExercices"
-		:key="exercice.id"
-		:exercice="exercice"
-		:index="parseInt(index)"
-		:type="'primary'"
-	/>
+		<button @click="isPrimaryExercices">Primary</button>
+		<button @click="isSecondaryExercices">Assistance</button>
+		<div v-if="isPrimarySelected">
+			<ExerciceLineItem 
+				v-for="(exercice, index) in this.primaryExercices"
+				:key="exercice.id"
+				:exercice="exercice"
+				:index="parseInt(index)"
+				:type="'primary'"
+			/>
+		</div>
+		<div v-else>
+			<ExerciceLineItem 
+				v-for="(exercice, index) in this.secondaryExercices"
+				:key="exercice.id"
+				:exercice="exercice"
+				:index="parseInt(index)"
+				:type="'secondary'"
+			/>
+		</div>
 	</div>
 </template>
 
 <script>
 import ExerciceLineItem from '@/components/ExerciceLineItem';
-// <ExerciceLineItem 
-// 			v-for="(exercice, index) in this.primaryExercices"
-// 			:key="exercice.id"
-// 			:exercices="this.primaryExercices"
-// 			:type="'primaryExercices'"
-// 			:index="parseInt(index)"
-// 		/>
 export default {
 	name: 'Set',
 	components: { ExerciceLineItem },
@@ -28,9 +34,16 @@ export default {
 	},
 	data () {
 		return {
+			isPrimarySelected: true
 		}
 	},
 	methods: {
+		isPrimaryExercices: function() {
+			this.isPrimarySelected = true
+		},
+		isSecondaryExercices: function() {
+			this.isPrimarySelected = false
+		}
 	}
 }
 </script>
