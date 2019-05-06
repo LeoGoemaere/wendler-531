74a6092d94dd4589f8fa7ad03fcb08c0063d657f
Goemaere Leo <goemaere.leo@hotmail.fr>
2019-05-06 13:09:09 +0200

    Add the other trainings on vue router

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,14 @@ import Router from 'vue-router'
 
 import Home from '@/components/sections/Home'
 import Training_1 from '@/components/sections/Training_1'
+import Training_2 from '@/components/sections/Training_2'
+import Training_3 from '@/components/sections/Training_3'
+import Training_4 from '@/components/sections/Training_4'
 
 Vue.use(Router)
 
 export default new Router({
+	mode: 'hash',
 	routes: [
 		{
 			path: '/',
@@ -16,6 +20,18 @@ export default new Router({
 		{
 			path: '/training-1',
 			component: Training_1
+		},
+		{
+			path: '/training-2',
+			component: Training_2
+		},
+		{
+			path: '/training-3',
+			component: Training_3
+		},
+		{
+			path: '/training-4',
+			component: Training_4
 		}
 	]
 })
