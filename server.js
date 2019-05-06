3dd090ed28b5cb00a3dd7b2db37142c46a9aad2d
Goemaere Leo <goemaere.leo@hotmail.fr>
2019-05-06 16:19:44 +0200

    Create server

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
+app.listen(port);
+
+console.log('server started...');
